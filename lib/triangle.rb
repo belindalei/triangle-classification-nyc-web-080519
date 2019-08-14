e91baf2f84c654fe279f538273750890c446ada3
Belinda Lei <belinda.lei95@gmail.com>
2019-08-14 15:06:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
   # write code here
+  attr_reader :lengthOne, :lengthTwo, :lengthThree
+  
+  def initialize(lengthOne, lengthTwo, lengthThree)
+    @lengthOne = lengthOne
+    @lengthTwo = lengthTwo
+    @lengthThree = lengthThree
+  end
+  
+  def kind
+    triangle?
+    if lengthOne == lengthTwo && lengthTwo == lengthThree
+      :equilateral
+    elsif lengthOne == lengthTwo || lengthTwo == lengthThree || lengthOne == lengthThree
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  
+  def triangle?
+    if lengthOne = 0 || lengthTwo = 0 || lengthThree = 0 
+      raise TriangleError
+    elsif lengthOne + lengthTwo < lengthThree && lengthThree + lengthOne < lengthTwo 
+      raise TriangleError
+    else 
+      puts "I'm a triangle!"
+    end
+  end 
+  
+  class TriangleError < StandardError 
+    def message 
+      "You have a Triangle Error" 
+    end
+  end
+  
 end
